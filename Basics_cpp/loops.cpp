ae6ed3cd2f54946c634acb23b5f83cc9e561f9c0
varshitfauzdar <varshitfauzdar2004@gmail.com>
2026-02-11 12:41:36 +0530

    practicing loops

---
+++ Basics_cpp/loops.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // for_loop
+    for(int i = 0;i<=5;i++ ){
+        cout<<"i am good"<<endl;
+    }
+    // while_loop
+    int a = 0;
+    while(a<=5){
+        cout<< "How are you\n";
+        a++;
+    }
+    // do_while
+    int n = 0;
+    do{
+        cout<< "i love india"<<endl;
+        n++;
+    } while (n <= 4);
+
+    return 0;
+    
+}
