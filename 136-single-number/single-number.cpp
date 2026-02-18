cb3ac0e0848e33744e04eef85f51bcfa58c44caf
varshit fauzdar <161210674+varshitfauzdar@users.noreply.github.com>
2026-02-18 19:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (82.65%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,21 +1,10 @@
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) {
-//         int result = 0;
-//         for(int i = 0; i< nums.size(); i++){
-//             result ^= nums[i];
-//         }
-//         return result;
-//     }
-// };
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int num : nums){
-            ans ^= num;
+        int result = 0;
+        for(int i = 0; i< nums.size(); i++){
+            result ^= nums[i];
         }
-        return ans;
+        return result;
     }
 };
