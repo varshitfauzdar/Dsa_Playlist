7f10cfc9a72bf091d1996aff2a431d1ba5c77665
varshit fauzdar <161210674+varshitfauzdar@users.noreply.github.com>
2026-02-24 12:19:51 +0530

    Time: 3 ms (68.01%) | Memory: 14.9 MB (36.00%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,16 +1,53 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map <int , int> mpp;
-        for(int i = 0; i<nums.size(); i++){
-            int a = nums[i];
-            int b = target - a;
+        // for(int i =0 ; i< nums.size();i++){
+        //     for(int j = i+1 ; j<nums.size();j++){
+        //         if(i == j){
+        //             continue;
+        //         }
+        //         else if(nums[i] + nums[j] == targett){
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // return {};
 
-            if(mpp.find(b) != mpp.end()){
-                return {mpp[b], i};;
+
+
+        //TWO POINTERS APPROACH
+        // sort(nums.begin(), nums.end());
+        // int i = 0;
+        // int j = nums.size()-1;
+        // while(i < j){
+        //     int sum = nums[i]+nums[j];
+
+        //     if(sum == target){
+        //         return {i,j};
+        //     }
+        //     else if(sum < target){
+        //         i++;
+        //     }
+        //     else{
+        //         j--;
+        //     }
+        // }
+
+
+        // USING HASHING
+        unordered_map<int, int> mp;  // value -> index
+
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
             }
-            mpp[a] = i;
-        }
-        return {};
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+
     }
 };
