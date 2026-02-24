ed4dc5901d6090adeebf5e3db63be7f61482d287
varshit fauzdar <161210674+varshitfauzdar@users.noreply.github.com>
2026-02-24 18:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.80%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -19,4 +19,26 @@ public:
             }
         }
     }
+
+    //brute force
+    // int c0 = 0,c1 = 0,
+    //     c2 = 0;
+
+    //     for(int i = 0; i< nums.size(); i++){
+    //         if(nums[i] == 0) c0++;
+    //         else if(nums[i] == 1) c1++;
+    //         else {
+    //             c2++;
+    //         }
+    //     }
+    //     for(int i = 0; i < c0; i++){
+    //         nums[i] = 0;
+    //     }
+    //     for(int i = c0 ; i < c0 + c1 ; i++){
+    //         nums[i] = 1;
+    //     }
+
+    //     for(int i = c0+c1 ; i < c0+c1+c2; i++){
+    //         nums[i] = 2;
+    //     }
 };
