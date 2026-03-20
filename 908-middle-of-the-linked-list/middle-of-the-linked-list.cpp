0299dcca0468387095217cbb10106ce2b59e9a13
varshit fauzdar <161210674+varshitfauzdar@users.noreply.github.com>
2026-03-20 17:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (25.85%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -3,7 +3,7 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        while (fast && fast->next) {
+        while (fast && fast->next) {  //as we know fast is travelling 2x..thats why we need to check null condition for last two element;
             slow = slow->next;
             fast = fast->next->next;
         }
