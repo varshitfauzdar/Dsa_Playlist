039eec26d024fffb0957c50ca6faf2b9fd014a9a
varshit fauzdar <161210674+varshitfauzdar@users.noreply.github.com>
2026-03-20 19:43:54 +0530

    Time: 8 ms (11.55%) | Memory: 76.8 MB (93.46%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -10,38 +10,80 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<int> s1, s2;
+    // ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {       // spcae = O(n) time = O(1)
+    //     stack<int> s1, s2;
 
-        while(l1){
-            s1.push(l1->val);
-            l1 = l1->next;
-        }
-        while(l2){
-            s2.push(l2->val);
-            l2 = l2->next;
-        }
+    //     while(l1){
+    //         s1.push(l1->val);
+    //         l1 = l1->next;
+    //     }
+    //     while(l2){
+    //         s2.push(l2->val);
+    //         l2 = l2->next;
+    //     }
 
+    //     int carry = 0;
+    //     ListNode* head = NULL;
+
+    //     while(!s1.empty() || !s2.empty() || carry){
+    //         int sum = carry;
+
+    //         if(!s1.empty()){
+    //             sum += s1.top(); s1.pop();
+    //         }
+    //         if(!s2.empty()){
+    //             sum += s2.top(); s2.pop();
+    //         }
+
+    //         ListNode* node = new ListNode(sum % 10);
+    //         node->next = head;
+    //         head = node;
+
+    //         carry = sum / 10;
+    //     }
+
+    //     return head;
+    // }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        while(head){
+            ListNode* nextNode = head->next;
+            head->next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {   // spcae = O(1) time = O(n)
+        // Step 1: Reverse both
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        // Step 2: Add like LC-2
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
         int carry = 0;
-        ListNode* head = NULL;
 
-        while(!s1.empty() || !s2.empty() || carry){
+        while(l1 || l2 || carry){
             int sum = carry;
 
-            if(!s1.empty()){
-                sum += s1.top(); s1.pop();
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(!s2.empty()){
-                sum += s2.top(); s2.pop();
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
             }
 
-            ListNode* node = new ListNode(sum % 10);
-            node->next = head;
-            head = node;
-
+            curr->next = new ListNode(sum % 10);
             carry = sum / 10;
+            curr = curr->next;
         }
 
-        return head;
+        // Step 3: Reverse result
+        return reverse(dummy->next);
     }
 };
